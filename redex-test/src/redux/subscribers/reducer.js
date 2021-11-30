926f155a37353e390ed91bd944917c60df974fc4
YoonMinSeok <81290520+YoonMinSeok@users.noreply.github.com>
2021-11-30 15:53:22 +0900

    Update redux-test

---
+++ redex-test/src/redux/subscribers/reducer.js
@@ -0,0 +1,22 @@
+import { ADD_SUBSCRIBER, REMOVE_SUBSCRIBER } from "./type"
+
+const initalState = {
+    count : 370
+}
+const subscribersReducer = (state=initalState, action) =>{
+    switch(action.type){
+        case ADD_SUBSCRIBER :
+            return{
+                ...state,
+                count : state.count +1
+            }
+        case REMOVE_SUBSCRIBER : 
+            return{
+                ...state,
+                count : state.count -1
+            }
+        default: return state
+    }
+}
+
+export default subscribersReducer;
